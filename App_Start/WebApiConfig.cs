660b45cbc49c6459cee7bf195409b27e68043d53
Thangamani.Durairaj <thangamani_d@yahoo.com>
2017-04-08 12:47:55 +0530

    binary files added

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace acad
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+               name: "DefaultApi1",
+               routeTemplate: "api/{controller}/{mod}/{id}",
+               defaults: new { mod = RouteParameter.Optional , id = RouteParameter.Optional, }
+           );
+
+
+        }
+    }
+}
